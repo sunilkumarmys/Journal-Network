bc2aed0004e01b8d8ec88907755689ebad9ba4ef
Sunil Kumar <sunilmkumar92@gmail.com>
2024-11-30 22:00:55 +0530

    first commit

---
+++ src/test/java/com/sunil/journal/JournalNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
